85e05e3330c6c503408db0d4aab9abd250600e95
FarhanABD <farhanabdilah204@gmail.com>
2024-07-07 11:06:34 +0700

    feat: Frontend template slicing

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,6 @@ use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Frontend\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
