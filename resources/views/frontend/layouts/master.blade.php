1a0efb442a8bf1014da36af332fc73378b55cc88
FarhanABD <farhanabdilah204@gmail.com>
2024-07-09 15:59:55 +0700

    feat : Frontend login register notification

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -22,6 +22,7 @@
     <link rel="stylesheet" href="{{asset('frontend/css/ranger_style.css')}}">
     <link rel="stylesheet" href="{{asset('frontend/css/jquery.classycountdown.css')}}">
     <link rel="stylesheet" href="{{asset('frontend/css/venobox.min.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
     <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
     <link rel="stylesheet" href="{{asset('frontend/css/responsive.css')}}">
@@ -133,9 +134,19 @@
     <script src="{{asset('frontend/js/venobox.min.js')}}"></script>
     <!--classycountdown js-->
     <script src="{{asset('frontend/js/jquery.classycountdown.js')}}"></script>
-
     <!--main/custom js-->
     <script src="{{asset('frontend/js/main.js')}}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    
+    <script>
+        @if ($errors->any())
+             @foreach ($errors->all() as $error)
+               toastr.error("{{$error}}")
+             @endforeach
+        @endif
+     </script>
+
 </body>
 
 </html>
