b27178c2d2d7f90a635b0587c775a72ba6f23e5c
FarhanABD <farhanabdilah204@gmail.com>
2024-07-13 20:40:37 +0700

    feat: User profile update feature

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\UserDashboardController;
+use App\Http\Controllers\Frontend\UserProfileController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -23,5 +24,6 @@ Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login'
 //-------------------- ROUTE GROUP FRONTEND USER ---------------------------------//
 Route::group(['middleware' =>['auth','verified'], 'prefix' => 'user', 'as' => 'user.'], function(){
     Route::get('dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+    Route::get('profile', [UserProfileController::class, 'index'])->name('profile');
 });
 //-------------------- ENDS OF ROUTE GROUP FRONTEND USER ---------------------------------//
