9297078a24b98732ef2c4a02730af3b5d00327fe
FarhanABD <farhanabdilah204@gmail.com>
2024-07-15 19:54:04 +0700

    feat: vendor Profile update

---
+++ app/Http/Controllers/Backend/VendorProfileController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class VendorProfileController extends Controller
+{
+    public function index(){
+        return view('vendor.dashboard.profile');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        //--------- VALIDASI DATA --------------//
+        $request->validate([
+            'name' => ['required', 'max:100'],
+            'email' => ['required', 'email','unique:users,email,'.Auth::user()->id],
+            'image' => ['image','max:2048']
+           ]);
+        //-------------------------------------// 
+        
+        //------ Kondisi menampilkan gambar default saat gambar kosong ----------//
+        $user = Auth::user();
+        if($request->hasFile('image')){
+            if(File::exists(public_path($user->image))){
+               File::delete(public_path($user->image));
+            }
+        //----------------------------------------------//
+        
+        //------------ FUNCTION UNTUK MENYIMPAN GAMBAR YANG DIUPLOAD -----------------//
+            $image = $request->image;
+            $imageName = rand().'_'.$image->getClientOriginalName();
+            $image->move(public_path('uploads'),$imageName);
+
+            $path = "/uploads/".$imageName;
+            $user->image = $path;
+        }
+
+        //--------- UPDATE USERNAME & EMAIL -------------//
+       $user->name = $request->name;
+       $user->email = $request->email;
+       $user->save();
+       toastr()->success('Profile updated succesfully');
+       return redirect()->back();
+    }
+
+    public function updatePassword(Request $request){
+        //--------- VALIDASI DATA PASSWORD USER --------------//
+        $request->validate([
+            'current_password' => ['required','current_password'],
+            'password' => ['required','confirmed', 'min:8'],
+        ]);
+    
+        $request->user()->update([
+            'password' => bcrypt($request->password)
+        ]);
+        toastr()->success('Password updated succesfully');
+        return redirect()->back();
+    }
+}
