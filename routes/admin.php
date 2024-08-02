23ade9c0292b11d223f2a7bc81994a1553735b60
FarhanABD <farhanabdilah204@gmail.com>
2024-08-02 21:03:16 +0700

    feat: Working with category index page

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SliderController;
 
@@ -15,3 +16,6 @@ Route::post('profile/update/password',[ProfileController::class, 'updatePassword
 
 //------------- SLIDER ROUTE --------------//
 Route::resource('slider', SliderController::class);
+
+//------------- CATEGORY ROUTE ---------------//
+Route::resource('category',CategoryController::class);
