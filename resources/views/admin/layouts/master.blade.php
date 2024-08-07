e0d61bb61dd3c169d420f8dae92834a09900a656
FarhanABD <farhanabdilah204@gmail.com>
2024-08-07 18:44:36 +0700

    feat: Subcategory - working with index page

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -20,6 +20,12 @@
   <link rel="stylesheet" href="https://cdn.datatables.net/2.1.0/css/dataTables.bootstrap5.css">
   <link rel="stylesheet" href="{{ asset('backend/assets/css/bootstrap-iconpicker.min.css') }}">
 
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+  integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
   <!-- Template CSS -->
   <link rel="stylesheet" href="{{asset('backend/assets/css/style.css')}}">
   <link rel="stylesheet" href="{{asset('backend/assets/css/components.css')}}">
--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SliderController;
+use App\Http\Controllers\Backend\SubCategoryController;
 
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
 
@@ -19,9 +20,8 @@ Route::resource('slider', SliderController::class);
 
 //------------- CATEGORY ROUTE ---------------//
 Route::put('change-status', [CategoryController::class, 'changeStatus'])->name('category.change-status');
-
-
-
-
-
 Route::resource('category',CategoryController::class);
+
+//------------- SUBCATEGORY ROUTE ---------------//
+Route::put('sub-category/change-status', [SubCategoryController::class, 'changeStatus'])->name('sub-category.change-status');
+Route::resource('sub-category',SubCategoryController::class);
